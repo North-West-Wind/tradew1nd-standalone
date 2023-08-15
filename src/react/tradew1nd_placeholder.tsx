2cc4bdbf06e48b03b8cb51d1e466674687a21a43
North-West-Wind <wsyn148@gmail.com>
2023-08-15 19:22:46 +0800

    provide markdown as help

--- src/react/tradew1nd_placeholder.tsx
+++ src/react/tradew1nd_placeholder.tsx
@@ -1,13 +1,37 @@
 import React from "react";
 import img from "../../public/images/tradew1nd.png";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import remarkGfm from "remark-gfm";
 
 export default class TradeW1ndPlaceholderComponent extends React.Component {
+	state: { markdown: string, showHelp: boolean };
+
+	constructor(props: object) {
+		super(props);
+		this.state = { markdown: "", showHelp: false };
+		fetch("https://raw.githubusercontent.com/North-West-Wind/tradew1nd-standalone/main/README.md").then(async res => {
+			if (res.ok) this.setState({ markdown: await res.text() });
+		});
+	}
+
+	toggleHelp() {
+		this.setState({ showHelp: !this.state.showHelp });
+	}
+
 	render() {
-		return <div className="flex-child center">
-			<img src={img} id="tradew1nd-placeholder" /><br/>
-			<h1>TradeW1nd Music Player</h1>
-			<h3>Made by NorthWestWind</h3>
-			<h3><a href="https://github.com/North-West-Wind/tradew1nd-standalone">GitHub Repository</a></h3>
-		</div>
+		return <>
+			<div className="flex-child center">
+				<img src={img} id="tradew1nd-placeholder" onClick={() => this.toggleHelp()} /><br/>
+				<h1>TradeW1nd Music Player</h1>
+				<h3>Made by NorthWestWind</h3>
+				<h3><a href="https://github.com/North-West-Wind/tradew1nd-standalone">GitHub Repository</a></h3>
+			</div>
+			{this.state.showHelp && <div className="overlay flex">
+				<div className="close-button" onClick={() => this.toggleHelp()}>x</div>
+				<div className="markdown">
+					<ReactMarkdown children={this.state.markdown} remarkPlugins={[remarkGfm]} />
+				</div>
+			</div>}
+		</>
 	}
 }
